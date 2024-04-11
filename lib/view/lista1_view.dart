581f21060ca6972ee7a11eadde00fe478e6509d9
nieledm <102603047+nieledm@users.noreply.github.com>
2024-04-11 20:05:00 -0300

    Editar item ok

--- lib/view/lista1_view.dart
+++ lib/view/lista1_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import '../model/lista_compras.dart'; // Importe a classe ListaCompras
 
@@ -122,7 +124,19 @@ class _Lista1ViewState extends State<Lista1View> {
                           setState(() {
                             dados.removeAt(index);
                           });
+                        } else if (result == 'editar') {
+                          setState(() {
+                            nomeController.clear();
+                            quantidadeController.clear();
+                            unidadeController.clear();
+
+                            nomeController.text = dados[index].item;
+                            quantidadeController.text = dados[index].quantidade.toString();
+                            unidadeController.text = dados[index].medida;
+                            _editarItem(context, index);
+                          });
                         }
+
                         // ELSE = LÓGICA PARA EDITAR ITEM
                       },
                       itemBuilder: (BuildContext context) =>
@@ -240,6 +254,83 @@ ADICIONAR ITEM
                 foregroundColor: Color.fromARGB(255, 253, 253, 253),
               ),
               child: Text('Adicionar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+/*#######################################################
+EDITAR ITEM
+#######################################################*/
+  void _editarItem(BuildContext context, index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Editar Item',
+            style: TextStyle(color: Colors.black),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: nomeController,
+                decoration: InputDecoration(
+                  labelText: 'Item',
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+              ),
+              TextFormField(
+                controller: quantidadeController,
+                decoration: InputDecoration(
+                  labelText: 'Quantidade',
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                controller: unidadeController,
+                decoration: InputDecoration(
+                  labelText: 'Unidade',
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey,
+                foregroundColor: Colors.white,
+              ),
+              child: Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Adicione aqui a lógica para adicionar o item à lista
+                String nome = nomeController.text;
+                double quantidade =
+                    double.tryParse(quantidadeController.text) ?? 0.0;
+                String unidade = unidadeController.text;
+                setState(() {
+                  dados[index] = (ListaCompras(nome, quantidade, unidade));
+                });
+                nomeController.clear();
+                quantidadeController.clear();
+                unidadeController.clear();
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(37, 28, 23, 0),
+                foregroundColor: Color.fromARGB(255, 253, 253, 253),
+              ),
+              child: Text('Editar'),
             ),
           ],
         );
